203a963ffeb6e2e0281d204fe016f65727d5f966
Godsave <pablomonie00@gmail.com>
2026-03-14 01:52:03 -0500

    dark mode added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,19 +4,22 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <ThemeProvider>
+      <div className="min-h-screen bg-black dark:bg-black light:bg-gray-50">
+        <Navbar />
+        <main>
+          <Hero />
+          <About />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
